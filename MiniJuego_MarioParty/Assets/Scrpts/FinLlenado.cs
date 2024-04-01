7be3b0105620553ab5f073a6176acd4e4d1aaaa9
Gerunad220 <rogerzon28@gmail.com>
2024-04-01 10:06:29 -0500

    Cambio en línea 12

---
+++ MiniJuego_MarioParty/Assets/Scrpts/FinLlenado.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinLlenado : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Invoke("LoadVuelo",3);
+    }
+
+    
+    public void LoadVuelo()
+    {
+        SceneManager.LoadScene("Vuelo");
+    }
+}
