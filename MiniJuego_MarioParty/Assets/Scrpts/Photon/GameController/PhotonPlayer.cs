dcb6fb1212fb43a4a4c8ccd7b50d9d1022c86a92
sofiapalas31 <k.sofia305@gmail.com>
2024-04-03 17:39:51 -0500

    Edición de scripts necesarios para modo multijugador

--- MiniJuego_MarioParty/Assets/Scrpts/Photon/GameController/PhotonPlayer.cs
+++ MiniJuego_MarioParty/Assets/Scrpts/Photon/GameController/PhotonPlayer.cs
@@ -1,13 +1,16 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PhotonPlayer : MonoBehaviour
 {
+    private PhotonView PV;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
