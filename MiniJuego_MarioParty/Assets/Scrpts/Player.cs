c943eb1e45ce79a69e04529c0b1e7b83675b9588
flansuru <lngiraldoo@unadvirtual.edu.co>
2024-04-05 14:49:32 -0500

    a

--- MiniJuego_MarioParty/Assets/Scrpts/Player.cs
+++ MiniJuego_MarioParty/Assets/Scrpts/Player.cs
@@ -1,20 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     public float speed = 5f;
-    
 
     private Rigidbody rb;
-    private bool isGrounded;
-
-    bool floorDetected = false;
-    bool isJump = false;
-    public float jumpForce = 5.0f;
 
     private void Awake()
     {
@@ -23,40 +16,11 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
-
-        
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         float moveVertical = Input.GetAxis("Vertical");
 
-        
-
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * speed * Time.deltaTime;
-        transform.Translate(movement);
-
-        Vector3 floor = transform.TransformDirection(Vector3.down);
-
-        if(Physics.Raycast(transform.position, floor, 1.2f))
-        {
-            floorDetected = true;
-            print("Contacto con el suelo");
-        }
-        else
-        {
-            floorDetected = false ;
-            print("No hay contacto con el suelo");
-        }
-
-
-        isJump = Input.GetButtonDown("Jump");
-
-        if (isJump && floorDetected)
-        {
-            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-        }
+        Vector3 movemet = new Vector3(moveHorizontal*speed,rb.velocity.y, moveVertical*speed);
+        rb.velocity = movemet;
     }
-
-   
-    
 }
